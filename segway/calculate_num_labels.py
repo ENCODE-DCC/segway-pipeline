f7ed314d2d475f10194945380e7b1ad79e8888fb
Paul Sud <41386393+p-sud@users.noreply.github.com>
2020-02-21 11:00:53 -0800

    PIP-928 calculate num labels (#3)

---
+++ segway/calculate_num_labels.py
@@ -0,0 +1,29 @@
+import argparse
+from math import sqrt
+
+
+def main():
+    parser = get_parser()
+    args = parser.parse_args()
+    num_labels = calculate_num_labels(args.num_tracks)
+    with open(args.outfile, "w") as f:
+        f.write(str(num_labels))
+
+
+def calculate_num_labels(num_tracks: int) -> int:
+    """
+    Calculates the number of labels based on the formula provided in Libbrecht et. al.
+    2019, 10 + 2 * sqrt(number of tracks)
+    """
+    return int(10 + 2 * int(sqrt(num_tracks)))
+
+
+def get_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--num-tracks", type=int, required=True)
+    parser.add_argument("-o", "--outfile", required=True)
+    return parser
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_calculate_num_labels.py
@@ -0,0 +1,34 @@
+import builtins
+from contextlib import suppress as does_not_raise
+from typing import List
+
+import pytest
+
+from segway.calculate_num_labels import calculate_num_labels, get_parser, main
+
+
+@pytest.mark.parametrize(
+    "args,condition",
+    [
+        (["--num-tracks", "3", "-o", "num_labels.txt"], does_not_raise()),
+        (["-o", "mylabels.txt"], pytest.raises(SystemExit)),
+        (["--num-tracks", "3"], pytest.raises(SystemExit)),
+    ],
+)
+def test_get_parser(args: List[str], condition):
+    parser = get_parser()
+    with condition:
+        parser.parse_args(args)
+
+
+def test_calculate_num_labels():
+    result = calculate_num_labels(16)
+    assert result == 18
+
+
+def test_main(mocker):
+    testargs = ["prog", "--num-tracks", "4", "-o", "numlabels.txt"]
+    mocker.patch("sys.argv", testargs)
+    mocker.patch("builtins.open", mocker.mock_open())
+    main()
+    assert builtins.open.mock_calls[2][1][0] == "14"
