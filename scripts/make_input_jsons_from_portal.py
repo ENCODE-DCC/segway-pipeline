c4e85bc834e9a1eb0997c2a97fd6c92cef800c5e
Shivek Narang <mrshiveknarang@gmail.com>
2020-09-14 14:43:08 -0700

    syntax error fixes

--- scripts/make_input_jsons_from_portal.py
+++ scripts/make_input_jsons_from_portal.py
@@ -351,7 +351,7 @@ def get_portal_files(
             raise ValueError(
                 f"Could not find all of the specified ChIP targets in the reference epigenome provided, missing {diff}"
             )
-    return list(datasets_files.values())
+    return list(datasets_files.values()),found_targets
 
 
 def make_input_json(portal_files: List[str], found_targets: List[str], extra_props: InputJson) -> InputJson:
