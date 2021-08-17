827f765942838a001462bf7fa4cee38bd7fd4304
Paul Sud <paulsud@stanford.edu>
2021-08-16 17:14:41 -0700

    lint

--- tests/functional/test_segway_full.py
+++ tests/functional/test_segway_full.py
@@ -5,9 +5,7 @@ import pytest
 
 @pytest.mark.workflow("test_segway_full")
 def test_segway_train_traindirs_match(test_data_dir, workflow_dir, traindirs_match):
-    actual_traindir_path = workflow_dir / Path(
-        "test-output/traindir.tar.gz"
-    )
+    actual_traindir_path = workflow_dir / Path("test-output/traindir.tar.gz")
     expected_traindir_path = test_data_dir / Path("segway_full_traindir.tar.gz")
     assert traindirs_match(actual_traindir_path, expected_traindir_path, workflow_dir)
 
