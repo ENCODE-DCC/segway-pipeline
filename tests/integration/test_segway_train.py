0069a41b0e05d1923f2b41d2cc2762ffc5ceb617
Paul Sud <paulsud@stanford.edu>
2021-08-17 16:51:57 -0700

    add segway train traindir

--- tests/integration/test_segway_train.py
+++ tests/integration/test_segway_train.py
@@ -6,5 +6,5 @@ import pytest
 @pytest.mark.workflow("test_segway_train")
 def test_segway_train_traindirs_match(test_data_dir, workflow_dir, traindirs_match):
     actual_traindir_path = workflow_dir / Path("test-output/traindir.tar.gz")
-    expected_traindir_path = test_data_dir / Path("traindir.tar.gz")
+    expected_traindir_path = test_data_dir / Path("segway_train_traindir.tar.gz")
     assert traindirs_match(actual_traindir_path, expected_traindir_path, workflow_dir)
